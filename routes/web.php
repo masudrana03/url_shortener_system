57de5dc461549925adc449862d287ca90c0c308b
masudrana03 <920mash@gmail.com>
2024-09-30 00:01:10 +0600

    laravel starter kits breeze compiled & layout template mastering done.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard1', function () {
+    return view('dashboard1');
+})->middleware(['auth', 'verified'])->name('dashboard1');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
